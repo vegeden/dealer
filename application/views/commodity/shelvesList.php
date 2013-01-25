3d7c53a6246dfd51b5ba14908b219107f39a5264
jhowe <wis.howe@gmail.com>
2013-01-25 15:12:26 +0800

    views/commodity/shelvesList.php 新增 js ckeditor 新增

---
+++ application/views/commodity/shelvesList.php
@@ -0,0 +1,47 @@
+<article id="storedvalue-lists">
+	<ul class="nav nav-pills pull-left">
+		<li <?php if($kind == 0) { ?> class="active" <?php } ?>>
+			<a href="<?php echo $url.'0/';?>"><?php echo $lang->line('commodity_shelves_stop_status');?></a>
+		</li>
+		<li <?php if($kind == 1) { ?> class="active" <?php } ?>>
+			<a href="<?php echo $url.'1/';?>"><?php echo $lang->line('commodity_shelves_sale_status');?></a>
+		</li>
+	</ul>	
+	<table class="table table-condensed table-bordered table-hover table-striped">
+		<tr class="info firstRow">
+			<td></td>
+			<td><?php echo $lang->line('commodity_item_name');?></td>
+			<td><?php echo $lang->line('commodity_item_content');?></td>
+		</tr>
+		<?php 
+			if($items_information_shelvers->num_rows() > 0) {
+				foreach($items_information_shelvers->result() as $row) {
+		?>
+		<tr class="info">		
+			<td>
+				<a href="/dealer/commodity/shelvesEditAdd/<?php echo $row->id;?>/"  rel="tooltip" title="<?php echo $lang->line('edit'); ?>"><img src="/dealer/statics/img/ic_action_edit.png"/></a>
+			</td>
+			<td><?php echo $row->item_name;?></td>
+			<td><?php echo $row->item_content;?></td>
+		</tr>
+		<?php 
+				}
+			}
+		?>
+	</table>
+	<?php if($page_TotalPageNum != 0) {?>
+	<div class="pagination pagination-centered">
+		<ul>
+			<li><a href="<?php echo $url.$kind.'/'.$page_previous.'/'; ?>">«</a></li>
+			<?php
+				for($i=1;$i<=$page_TotalPageNum;$i++) {
+			?>
+			<li><a href="<?php echo $url.$kind.'/'.$i.'/';?>"><?php echo $i;?></a></li>
+			<?php 
+				}
+			?>
+			<li><a href="<?php echo $url.$kind.'/'.$page_next.'/'; ?>">»</a></li>
+		</ul>
+	</div>
+	<?php } ?>
+</article>
