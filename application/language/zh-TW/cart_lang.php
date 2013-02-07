6a7101462a8b88cdc3f2f74ad83f345025040b4e
jiunjiun <quietmes@gmail.com>
2013-02-07 17:19:45 +0800

    edit cart language add SWheres function in items_information.php

--- application/language/zh-TW/cart_lang.php
+++ application/language/zh-TW/cart_lang.php
@@ -1,6 +1,8 @@
 <?php
-	$lang['account_Error']						= 'Error!!';
-	
+	$lang['store_dollar_sign']				= '$';
+	$lang['cart_stock_quantity']			= '尚有：';
+	$lang['cart_remove']					= '移除';
+	$lang['cart_sum']						= '訂單總計';
 	
 	
 	
