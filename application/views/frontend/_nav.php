ea208256d3db91756146d8c27b857ffc5241e61b
jiunjiun <quietmes@gmail.com>
2013-02-21 09:47:46 +0800

    Edit nav style

--- application/views/frontend/_nav.php
+++ application/views/frontend/_nav.php
@@ -8,14 +8,14 @@
 					foreach($nav->result() as $rows) {
 			?>
 			<li>
-				<a href="#"><?php echo $rows->category_name; ?></a> 
+				<a href="www.google.com"><?php echo $rows->category_name; ?></a> 
 				<ul>
 					<?php 
 						$nav_second = $this->items_category_second->SWhereCategory($rows->id);
 						if(isset($nav_second)) if($nav_second->num_rows() > 0) 
-							foreach($nav_second->result() as $row) {	
+							foreach($nav_second->result() as $key => $row) {	
 					?>
-					<li><a href="#"><?php echo $row->category_second_name; ?></a></li>
+					<li class="<?php if($key == count($nav_second->result())-1) echo "last";?>"><a href="#1"><?php echo $row->category_second_name; ?></a></li>
 					<?php } ?>
 				</ul>
 			</li>
