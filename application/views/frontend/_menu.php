a71259b2b0859d6f39df6f5698fec3e43f75599b
jiunjiun <quietmes@gmail.com>
2013-02-21 16:13:00 +0800

    區分類別是特賣或 second nav

--- application/views/frontend/_menu.php
+++ application/views/frontend/_menu.php
@@ -1,2 +1,14 @@
-<div class="nav_second"></div>
-<div class="advertisement"></div>
+<div id="nav_second">
+	<div id="title" class="gradient_BlackWhite">
+		<?php echo preg_match('/store/', uri_string()) ? $lang->line('nav_class') : $lang->line('specialoffer') ?>
+	</div>
+	<?php
+		if(preg_match('/store/', uri_string())) 
+			require_once '_menu_nav_second.php';
+		else 
+			require_once '_menu_specialoffer.php';
+	?>
+</div>
+<div class="advertisement">
+	<img src="/<?php echo $lang->line('folder_name').'/statics/img_advertisement/ad_left.jpg';?>">
+</div>
---
+++ application/views/frontend/_menu_nav_second.php
@@ -0,0 +1,5 @@
+<ul>
+	<li>加購商品</li>
+	<li>減碳活動專區</li>
+	<li class="last">新品上架</li>
+</ul>
