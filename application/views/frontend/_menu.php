5f470751e193f96f20bcd7552342f0b315c02a29
jiunjiun <quietmes@gmail.com>
2013-02-23 10:41:34 +0800

    Reset regular expression set new regular expression link profile page

--- application/views/frontend/_menu.php
+++ application/views/frontend/_menu.php
@@ -1,12 +1,23 @@
 <div id="nav_second">
 	<div id="title" class="gradient_BlackWhite">
-		<?php echo preg_match('/store$/', uri_string()) ? $lang->line('nav_class') : $lang->line('specialoffer') ?>
+		<?php 
+			if(preg_match('/store$/', uri_string())) {
+				echo $lang->line('nav_class');
+			} else if(preg_match('/profile$/', uri_string())) {
+				echo $lang->line('my_profile');
+			} else {
+				echo $lang->line('specialoffer');
+			}
+		?>
 	</div>
 	<?php
-		if(preg_match('/store$/', uri_string())) 
+		if(preg_match('/store$/', uri_string())) {
 			require_once '_menu_nav_second.php';
-		else 
+		} else if(preg_match('/profile$/', uri_string())) {
+			require_once '_menu_profile.php';
+		} else {
 			require_once '_menu_specialoffer.php';
+		}
 	?>
 </div>
 <div class="advertisement">
