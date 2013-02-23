36ed0700d7da305b0b65d985379ebf44c8f79d2f
jiunjiun <quietmes@gmail.com>
2013-02-23 09:34:34 +0800

    Edit regular expression

--- application/views/frontend/_menu.php
+++ application/views/frontend/_menu.php
@@ -1,9 +1,9 @@
 <div id="nav_second">
 	<div id="title" class="gradient_BlackWhite">
-		<?php echo preg_match('/store/', uri_string()) ? $lang->line('nav_class') : $lang->line('specialoffer') ?>
+		<?php echo preg_match('/store$/', uri_string()) ? $lang->line('nav_class') : $lang->line('specialoffer') ?>
 	</div>
 	<?php
-		if(preg_match('/store/', uri_string())) 
+		if(preg_match('/store$/', uri_string())) 
 			require_once '_menu_nav_second.php';
 		else 
 			require_once '_menu_specialoffer.php';
