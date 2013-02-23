ac56e33fae8558c96603a430b135de6ea9606c1a
jiunjiun <quietmes@gmail.com>
2013-02-23 10:30:18 +0800

    String word change link language file word.

--- application/views/frontend/_menu_nav_second.php
+++ application/views/frontend/_menu_nav_second.php
@@ -1,5 +1,4 @@
 <ul>
-	<li>加購商品</li>
-	<li>減碳活動專區</li>
-	<li class="last">新品上架</li>
+	<li></li>
+	<li class="last"></li>
 </ul>
--- application/views/frontend/_menu_specialoffer.php
+++ application/views/frontend/_menu_specialoffer.php
@@ -1,5 +1,5 @@
 <ul>
-	<li></li>
-	<li></li>
-	<li class="last"></li>
+	<li><?php echo $lang->line('menu_purchase_of_goods'); ?></li>
+	<li><?php echo $lang->line('menu_energy_saving_activity'); ?></li>
+	<li class="last"><?php echo $lang->line('menu_new_store'); ?></li>
 </ul>
