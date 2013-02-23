305257efdc2a29cfe6eee09074ddc00ddbe1c335
jiunjiun <quietmes@gmail.com>
2013-02-23 17:48:10 +0800

    Reset regular expression

--- application/views/frontend/_menu.php
+++ application/views/frontend/_menu.php
@@ -1,9 +1,9 @@
 <div id="nav_second">
 	<div id="title" class="gradient_BlackWhite">
 		<?php 
-			if(preg_match('/store$/', uri_string())) {
+			if(preg_match('/store\/*/', uri_string())) {
 				echo $lang->line('nav_class');
-			} else if(preg_match('/profile$/', uri_string())) {
+			} else if(preg_match('/profile\/*/', uri_string())) {
 				echo $lang->line('my_profile');
 			} else {
 				echo $lang->line('specialoffer');
@@ -11,9 +11,9 @@
 		?>
 	</div>
 	<?php
-		if(preg_match('/store$/', uri_string())) {
+		if(preg_match('/store\/*/', uri_string())) {
 			require_once '_menu_nav_second.php';
-		} else if(preg_match('/profile$/', uri_string())) {
+		} else if(preg_match('/profile\/*/', uri_string())) {
 			require_once '_menu_profile.php';
 		} else {
 			require_once '_menu_specialoffer.php';
