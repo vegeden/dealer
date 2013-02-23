3b42dd7a79cc786f773c78ba5c6fd85fd82aa9a4
jiunjiun <quietmes@gmail.com>
2013-02-23 10:27:19 +0800

    Add menu for user setting add _menu_user_setting.php

---
+++ application/views/frontend/_menu_user_setting.php
@@ -0,0 +1,5 @@
+<ul>
+	<li><?php echo $lang->line('menu_profile'); ?></li>
+	<li><?php echo $lang->line('menu_account_settings'); ?></li>
+	<li class="last"><?php echo $lang->line('menu_account_settings'); ?></li>
+</ul>
