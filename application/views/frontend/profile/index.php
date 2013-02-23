129167a1dca9df29b100000975cc7dc13541ea58
jiunjiun <quietmes@gmail.com>
2013-02-23 17:50:54 +0800

    Add editProfile.php from profile

---
+++ application/views/frontend/profile/index.php
@@ -0,0 +1,12 @@
+<article id="profile-index" class="profile">
+	<h2><?php echo $lang->line('storedvalue_plus');?></h2>
+	<div id="declaration" class="center">
+		
+	</div>
+	<div id="submit" class="center" >
+		<form method="POST" action="">
+			<button id="NotApply" class="btn btn-large" type="sumbit" name="cancel" value="cancel"><?php echo $lang->line('storedvalue_NotApply');?></button>
+			<button id="apply" class="btn btn-large btn-success" type="apply" name="apply" value="apply"><?php echo $lang->line('storedvalue_apply');?></button>
+		</form>
+	</div>
+</article>
