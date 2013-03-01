be0088eb1fde697aa3c2929951f086b60f128385
jiunjiun <quietmes@gmail.com>
2013-03-01 17:13:07 +0800

    Edit store javascript

--- statics/js/frontend/store.js
+++ statics/js/frontend/store.js
@@ -1,4 +1,8 @@
 $(function(){
+	/**		index		**/
+	// $(".price-num").attr("data-price",$(this).html());
+	// $(".price-num").priceFormat({prefix: '', centsSeparator: ',', centsLimit: 3});
+	
 	/**		commodity		**/
 	$('#commodity a').click(function() {
 		var i = $(this).attr('href');
