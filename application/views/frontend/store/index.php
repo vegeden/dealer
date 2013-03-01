95494a97ec443deaf5668be30845f9bdc48635e0
jiunjiun <quietmes@gmail.com>
2013-03-01 17:10:30 +0800

    Edit store index html

--- application/views/frontend/store/index.php
+++ application/views/frontend/store/index.php
@@ -1,10 +1,176 @@
-<article id="store" class="center">	
+<article id="store">
+	<div id="top4" class="side">
+		<div id="title" class="gradient_BlackWhite"><?php echo $lang->line('store_top4');?></div>
+		<ul>
+			<li>
+				<a class="thumbnail" href="#">
+					<img src="" data-src="holder.js/220x220" alt="">
+					<div id="name" class="center">你好你好你好</div>
+					<div class="price center">
+						<span>特價：</span>
+						<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="thumbnail" href="#">
+					<img src="" data-src="holder.js/220x220" alt="">
+					<div id="name" class="center">你好你好你好</div>
+					<div class="price center">
+						<span>特價：</span>
+						<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="thumbnail" href="#">
+					<img src="" data-src="holder.js/220x220" alt="">
+					<div id="name" class="center">你好你好你好</div>
+					<div class="price center">
+						<span>特價：</span>
+						<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a class="thumbnail" href="#">
+					<img src="" data-src="holder.js/220x220" alt="">
+					<div id="name" class="center">你好你好你好</div>
+					<div class="price center">
+						<span>特價：</span>
+						<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div id="order" class="side">
+		<ul>
+			<li>排序：</li>
+			<li><a class="select" href="#">最新上架</a><span class="divider"> |&nbsp;</span></li>
+			<li><a href="#">售價低到高</a><span class="divider"> |&nbsp;</span></li>
+			<li><a href="#">售價高到低</a></li>
+		</ul>
+	</div>
+	<div class="content side">
+		<div id="title" class="gradient_BlackWhite">營養補給</div>
+		<ul>
+			<li>
+				<ul>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div class="content">
+		<ul>
+			<li>
+				<ul>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a class="thumbnail" href="#">
+							<img src="" data-src="holder.js/180x180" alt="">
+							<div id="name" class="center">你好你好你好</div>
+							<div class="price center">
+								<span>特價：</span>
+								<span class="price-num" data-price="4800"><?php echo number_format(4800); ?></span>
+							</div>
+						</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
 	<?php 
 	switch($store_level){
-		case 0:
-	?>
-	<?php
-			break;
 		case 1:
 			if($category_second->num_rows()>0)
 			foreach($category_second->result() as $key_category_second => $row_category_second) {				
@@ -52,3 +218,4 @@
 	}
 	?>
 </article>
+<script src="/dealer/statics/js/lib/Jquery-Price-Format/jquery.price_format.js"></script>
