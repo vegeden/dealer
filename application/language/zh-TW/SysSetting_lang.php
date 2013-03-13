9e0846ab1a95be8a1fd9f8cbfc0ee9643336b79a
jiunjiun <quietmes@gmail.com>
2013-03-13 15:47:30 +0800

    Edit SysSetting_lang.php Add 建立/修改 註解文字

--- application/language/zh-TW/SysSetting_lang.php
+++ application/language/zh-TW/SysSetting_lang.php
@@ -13,7 +13,9 @@
 	$lang['SysSetting_publicPage0']				= '顯示';
 	$lang['SysSetting_publicPage1']				= '隱藏';
 	
-	/*	level Add */
+	$lang['SysSetting_permission_note']			= '請輸入顯示權限的user_type id(有兩個以上以逗點隔開)';
+	$lang['SysSetting_publicPage_note']			= '請輸入0和1(0為顯示, 1為隱藏)';
+	
 	$lang['account_levelAdd_myself']			= '自己';
 	$lang['account_levelAdd_ErrorMsg']			= '類別重複';
 	
