7e27bd3be4524972acd3428e2a974a32f36eb9a1
jiunjiun <quietmes@gmail.com>
2013-03-13 15:28:31 +0800

    Add ACL.php, ACLEdit.php, _formACL page

---
+++ application/views/backend/SysSetting/_formACL.php
@@ -0,0 +1,16 @@
+		<dl class="dl-horizontal dlform level">
+			<dt><?php echo $lang->line('SysSetting_group'); ?></dt>
+			<dd><input type="text" placeholder="" class="input-large" name="group" value="<?php if(!empty($ACL->group))echo $ACL->group;?>"></dd>
+			<dt><?php echo $lang->line('SysSetting_access'); ?></dt>
+			<dd><input type="text" placeholder="" class="input-large" name="access" value="<?php if(!empty($ACL->access))echo $ACL->access;?>"></dd>
+			<dt><?php echo $lang->line('SysSetting_permission'); ?></dt>
+			<dd><input type="text" placeholder="" class="input-large" name="permission" value="<?php if(!empty($ACL->permission))echo $ACL->permission;?>"></dd>
+			<dt><?php echo $lang->line('SysSetting_publicPage'); ?></dt>
+			<dd><input type="text" placeholder="" class="input-large" name="publicPage" value="<?php if(isset($ACL->publicPage))echo $ACL->publicPage;?>"></dd>
+		</dl>
+		<div class="row">
+			<div class="span3 offset3">
+				<button type="submit" class="btn btn-primary"><?php echo $lang->line('submit'); ?></button>
+				<button type="submit" class="btn offset2" name="cancel" value="cancel"><?php echo $lang->line('cancel'); ?></button>
+			</div>
+		</div>
