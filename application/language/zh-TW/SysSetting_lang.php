02e47325e79d0a43d0282b94321e0f5285499972
jiunjiun <quietmes@gmail.com>
2013-03-13 15:26:24 +0800

    Edit SysSetting_lang.php language

--- application/language/zh-TW/SysSetting_lang.php
+++ application/language/zh-TW/SysSetting_lang.php
@@ -6,6 +6,12 @@
 	$lang['SysSetting_directions']				= 'Directions';
 	$lang['SysSetting_dateTime']				= 'UpdateTime';
 	
+	$lang['SysSetting_group']					= '群組';
+	$lang['SysSetting_access']					= '頁面';
+	$lang['SysSetting_permission']				= '權限';
+	$lang['SysSetting_publicPage']				= '顯示在Menu';
+	$lang['SysSetting_publicPage0']				= '顯示';
+	$lang['SysSetting_publicPage1']				= '隱藏';
 	
 	/*	level Add */
 	$lang['account_levelAdd_myself']			= '自己';
