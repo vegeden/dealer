5836de8ec96c91d99764e4489a967619e4ac8b7f
jiunjiun <quietmes@gmail.com>
2013-03-15 11:38:22 +0800

    Edit cart.js if not data display no_data div view

--- statics/js/frontend/cart.js
+++ statics/js/frontend/cart.js
@@ -8,8 +8,13 @@ $(function(){
 		},function(request){
 			var org = $('ul.media-list li').first().find('dl a').attr('href');
 			opt.parent().parent().parent().parent().parent().remove();
-			if(org == i) $('ul.media-list li');//.first().removeClass('hr');		
+			if(org == i) $('ul.media-list li');//.first().removeClass('hr');	
+			if($('ul.media-list li').size() == 0) {
+				$('article#cart div#cart-info').html('');
+				$('div#no_data').removeClass('displaynNone');
+			}
 		});
+		
 		return false;
 	});
 	
@@ -17,26 +22,31 @@ $(function(){
 		var opt = $(this);
 		var one_price 	= $(this).parent().parent().find('.one_price').attr('data-price');
 		var number 		= $(this).val();
+		
 		// input buffer lock
 		opt.prop('disabled', true);
 		setTimeout(function() {
-			// update price
-			var pull_pot = opt.parent().parent();
-			var old = pull_pot.find('.total_price').attr('data-price');
-				pull_pot.find('.total_price').attr('data-price',one_price*number).val(one_price*number)
-						.priceFormat({prefix: '', centsSeparator: ',', centsLimit: 3});
-			
-			// update sum price;
-			var sum = $('div.row dl dd').attr('data-price')-old+(one_price*number);
-			$('div.row dl dd').attr('data-price',sum).val(sum).priceFormat({prefix: '', centsSeparator: ',', centsLimit: 3});
+			if(number == 0) {
+				opt.parent().parent().parent().parent().parent().find('a.remove').trigger('click');
+			} else {
+				// update price
+				var pull_pot = opt.parent().parent();
+				var old = pull_pot.find('.total_price').attr('data-price');
+					pull_pot.find('.total_price').attr('data-price',one_price*number).val(one_price*number)
+							.priceFormat({prefix: '', centsSeparator: ',', centsLimit: 3});
+				
+				// update sum price;
+				var sum = $('div.row dl dd').attr('data-price')-old+(one_price*number);
+				$('div.row dl dd').attr('data-price',sum).val(sum).priceFormat({prefix: '', centsSeparator: ',', centsLimit: 3});
 
-			// find id;
-			var i = pull_pot.parent().parent().find('dl dd a').attr('href');
-			// update data
-			$.post('/dealer/cart/add/',{'i' : i, 'c' : number});
-			
-			// input buffer open
-			opt.prop('disabled', false);
+				// find id;
+				var i = pull_pot.parent().parent().find('dl dd a').attr('href');
+				// update data
+				$.post('/dealer/cart/add/',{'i' : i, 'c' : number});
+				
+				// input buffer open
+				opt.prop('disabled', false);
+			}
 		}, 400);
 	});
 	
