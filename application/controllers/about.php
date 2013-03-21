d5a25c52e04c91a392438697bfe6949a87938bcb
jiunjiun <quietmes@gmail.com>
2013-03-21 15:25:19 +0800

    Create controllers/about.php, zh-TW/about_lang.php

---
+++ application/controllers/about.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class About extends CI_Controller {
+	private $parames, $UserInfo;
+	private $Url;
+	
+    function __construct() {
+        parent::__construct();
+		
+		$this->load->model('Parames');
+		$this->UserInfo = $this->Parames->getUserInfo();
+		
+		$this->Url = '/'.$this->lang->line('folder_name').'/'.strtolower(get_class($this)).'/';
+    }	
+	
+	public function index() {
+		/*	-------------------------------------------	*/
+		$this->Parames->init('nav_about_index');
+		$this->parames = $this->Parames->getParams();
+		$this->parames['url'] = $this->Url.__FUNCTION__.'/';
+		/*	-------------------------------------------	*/
+		$this->load->view('index', $this->parames);
+	}
+}
+
+
+
+
+
