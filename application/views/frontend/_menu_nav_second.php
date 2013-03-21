fc8b15aa242653ae6be5f4c12abd08e6420bba12
jiunjiun <quietmes@gmail.com>
2013-03-21 10:19:19 +0800

    Edit _menu_nav_second.php Add display second nav

--- application/views/frontend/_menu_nav_second.php
+++ application/views/frontend/_menu_nav_second.php
@@ -1,4 +1,8 @@
 <ul>
-	<li>nav</li>
-	<li class="last">second</li>
+	<?php 
+		if(isset($menu_nav_second)) if($menu_nav_second->num_rows() > 0) 
+			foreach($menu_nav_second->result() as $key => $row) {	
+	?>
+	<li class="<?php if($key == count($menu_nav_second->result())-1) echo "last";?>"><a href="/<?php echo $lang->line('folder_name').'/store/index/'.$category.'/'.$row->id.'/';?>"><?php echo $row->category_second_name; ?></a></li>
+	<?php 	} ?>
 </ul>
