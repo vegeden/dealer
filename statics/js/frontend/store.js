dfaf95c99851da0c085138415c939bad8d5e0d9f
jiunjiun <quietmes@gmail.com>
2013-03-21 11:14:01 +0800

    Edit frontend/store.js 修改在store index js會error, 在js include toastr js, css lib, 並修改toastr.option

--- statics/js/frontend/store.js
+++ statics/js/frontend/store.js
@@ -10,15 +10,15 @@ $(function(){
 		});
 	}();
 	
-	/**		index		**/
-	toastr.options = {timeOut: 1000, positionClass: 'toast-bottom-right'};
-	
 	/**		commodity		**/
+	IncludeJavaScript('/dealer/statics/lib/toastr/toastr.min.js');
+	IncludeCSS('/dealer/statics/lib/toastr/toastr.css');
 	$('#store-commodity a').click(function() {
 		var i = $(this).attr('href');
 		$.post('/dealer/cart/add/',{
 			'i' : i
 		},function(request){
+			toastr.options = {timeOut: 1000, positionClass: 'toast-bottom-right'};
 			toastr.success(lang.language['add_cart']);
 		});
 		return false;
