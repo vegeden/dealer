3080eee6f6a556b0660eefe35e41ced988a06717
jiunjiun <quietmes@gmail.com>
2013-04-26 12:20:46 +0800

    Update view/forntend/store/index

--- application/views/frontend/store/index.php
+++ application/views/frontend/store/index.php
@@ -9,7 +9,7 @@
 			?>
 			<li>
 				<a class="thumbnail" href="/dealer/store/commodity/<?php echo $row_store->id ;?>/">
-					<img src="/dealer/img/store/<?php echo $row_store->id ;?>" alt="">
+					<img src="/dealer/img/store/<?php echo $row_store->id ;?>" alt="" width="220" height="220">
 					<div id="name" class="center"><?php echo $row_store->item_name ;?></div>
 					<div class="price center">
 						<span><?php echo $lang->line('store_special_offer'); ?></span>
@@ -42,7 +42,7 @@
 					?>
 					<li>
 						<a class="thumbnail" href="/dealer/store/commodity/<?php echo $row_store->id ;?>/">
-							<img src="/dealer/img/store/<?php echo $row_store->id ;?>" alt="">
+							<img src="/dealer/img/store/<?php echo $row_store->id ;?>" alt="" width="150" height="150">
 							<div id="name" class="center"><?php echo $row_store->item_name ;?></div>
 							<div class="price center">
 								<span><?php echo $lang->line('store_special_offer'); ?></span>
