625d1882877d1c16f1d1d3343ad8bd0a5cb6a6fa
jiunjiun <quietmes@gmail.com>
2013-04-26 16:20:30 +0800

    Update language/store Add new word and 排版

--- application/language/zh-TW/store_lang.php
+++ application/language/zh-TW/store_lang.php
@@ -1,20 +1,16 @@
 <?php
-	$lang['store_dollar_sign']				= '$';
-	
-	$lang['store_sell']						= '售價';
-	$lang['store_freight_price']			= '運費';
-	$lang['store_special_item']				= '特殊商品';
-	$lang['store_special_explain']			= '(該商品將設定為獨立運費)';
-	$lang['store_add_Cart']					= '加入購物車';
-	
-	$lang['store_top4']						= '熱門商品';
-	
-	$lang['content_title']					= '商品內容';
-	
-	$lang['add_cart']						= '已加入購物車';
-	
-	$lang['store_special_offer']			= '特價：';
-	$lang['store_order']					= '排序：';
-	$lang['store_new_store']				= '最新上架';
-	$lang['store_order_low_to_hight']		= '售價低到高';
-	$lang['store_order_hight_to_low']		= '售價高到低';
+	$lang['store_dollar_sign']        = '$';
+	$lang['store_sell']               = '售價';
+	$lang['store_freight_price']      = '運費';
+	$lang['store_special_item']       = '特殊商品';
+	$lang['store_special_explain']    = '(該商品將設定為獨立運費)';
+	$lang['store_add_Cart']           = '加入購物車';
+	$lang['store_commodity_id']       = '商品編號: ';
+	$lang['store_top4']               = '熱門商品';
+	$lang['content_title']            = '商品內容';
+	$lang['add_cart']                 = '已加入購物車';
+	$lang['store_special_offer']      = '特價：';
+	$lang['store_order']              = '排序：';
+	$lang['store_new_store']          = '最新上架';
+	$lang['store_order_low_to_hight'] = '售價低到高';
+	$lang['store_order_hight_to_low'] = '售價高到低';
